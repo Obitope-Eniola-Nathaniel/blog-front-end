26140f6768e9afbbc8c2c62dfe2c4b8a94f3501e
obitopeeniola <obitopeeniola@gmail.com>
2025-06-18 14:02:48 +0100

    showblog people

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 // Home.js
 import React from "react";
 import "./Home.css"; // Assuming you have a CSS file for styling
+import image from "../images/peoplecom.jpg"; // Adjust the path as necessary
 const Home = () => {
     
     return (
@@ -12,6 +13,10 @@ const Home = () => {
                     <p>Explore our features and start your blogging journey today!</p>
                     <p>Login or Register to get started.</p>
                     <div className="happy-blogging">Happy Blogging!</div>
+                </section>
+                <section className="features">
+                    <h2>Show Case Your Creativity</h2>
+                    <img src={image} alt="Showcase" className="image-people"/>
                 </section>
             </main>
         </div>
